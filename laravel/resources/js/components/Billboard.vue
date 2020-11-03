b4177d435d4c27e628f4b0f7fa7135703bd4dc25
danimnh <danimnh4@gmail.com>
2020-11-04 00:36:40 +0700

    add : title, headings, mockup billboard

---
+++ laravel/resources/js/components/Billboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img
+                    class="billboard"
+                    src="../../../public/billboard-bg.jpg"
+                    alt=""
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
---
+++ laravel/resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Kolokium Eclipse54</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body style='background-color: white'>
+    <main id="app">
+    <div class="container-fluid p-4">
+        <div class="row">
+            <div class="col-md"><headertitle/></div>
+            <div class="col-md"><billboard/></div>
+        </div>
+    </div>
+    <div class="p-4 flex-row">
+            
+    </div>
+    </main>
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
