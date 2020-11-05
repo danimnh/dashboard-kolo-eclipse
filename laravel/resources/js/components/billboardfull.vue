a86c2c4a26128706a4b312abab0e58f4b95c8723
danimnh <danimnh4@gmail.com>
2020-11-06 00:06:51 +0700

    mockup : change layout

---
+++ laravel/resources/js/components/billboardfull.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <billboard-top />
+        <billboard-1 />
+        <billboard-2 />
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
